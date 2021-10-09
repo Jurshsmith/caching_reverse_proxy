a54779dc61d382fc5f3e64401a6fb803733beadd
Joshua Oladele <jurshsmith@gmail.com>
2021-10-09 22:07:55 +0100

    fixes naming

--- lib/caching_reverse_proxy.ex
+++ lib/caching_reverse_proxy.ex
@@ -6,25 +6,25 @@ defmodule CachingReverseProxy do
   # Every cache needs to implement a private resolve_from_source function
   alias CachingReverseProxy.Cache
 
-  def resolve_with_caching(key, resolve_from_source!) do
+  def resolve_with_caching(key, resolve_from_source) do
     Cache.lookup(key)
-    |> resolve_cache(key, resolve_from_source!)
+    |> resolve_cache(key, resolve_from_source)
   end
 
   defp resolve_cache(
          %{time_stamp: cache_time_stamp, value: cache_value},
          _key,
-         resolve_from_source!
+         resolve_from_source
        ) do
-    with true <- Cache.has_expired?(cache_time_stamp), {:ok, data} <- resolve_from_source!.() do
+    with true <- Cache.has_expired?(cache_time_stamp), {:ok, data} <- resolve_from_source.() do
       {:ok, data}
     else
       _ -> {:ok, cache_value}
     end
   end
 
-  defp resolve_cache(nil, key, resolve_from_source!) do
-    case resolve_from_source!.() do
+  defp resolve_cache(nil, key, resolve_from_source) do
+    case resolve_from_source.() do
       {:ok, data} ->
         Cache.cache(key, data)
         {:ok, data}
