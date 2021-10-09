1debb5963bc065e4c51f395f8707a406dbc9cdec
Joshua Oladele <jurshsmith@gmail.com>
2021-10-09 22:09:23 +0100

    updates naming

--- lib/caching_reverse_proxy/supported_services/npm/resolver.ex
+++ lib/caching_reverse_proxy/supported_services/npm/resolver.ex
@@ -3,10 +3,10 @@ defmodule CachingReverseProxy.NPM.Resolver do
 
   def resolve(_parent, %{name: name}, _resolution) do
     name
-    |> CachingReverseProxy.resolve_with_caching(fn -> resolve_from_source!(name) end)
+    |> CachingReverseProxy.resolve_with_caching(fn -> resolve_from_source(name) end)
   end
 
-  defp resolve_from_source!(name) do
+  defp resolve_from_source(name) do
     Adapter.fetch_npm_package_description(name)
   end
 end
